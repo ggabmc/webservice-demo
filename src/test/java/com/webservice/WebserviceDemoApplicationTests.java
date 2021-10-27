5caf1388c23588595d2565fe5085598e75a333ff
Gabriel <ggabmc@hotmail.com>
2021-10-27 16:30:57 -0700

    travis file

--- src/test/java/com/webservice/WebserviceDemoApplicationTests.java
+++ src/test/java/com/webservice/WebserviceDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.webservice;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,12 @@ class WebserviceDemoApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void anotherTest(){
+        Assertions.assertTrue(true);
     }
 
 }
