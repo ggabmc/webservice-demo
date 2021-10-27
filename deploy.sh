55e8877f6c9adf9f2e0c3e623a0f9bf6ff8db0c8
Gabriel <ggabmc@hotmail.com>
2021-10-26 22:19:21 -0700

    travis file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-docker build -t ggabmc/webservice-demo ./Dockerfile ./webservice-demo
-
-docker push ggabmc/webservice-demo
+#docker build -t ggabmc/webservice-demo ./Dockerfile ./webservice-demo
+#
+#docker push ggabmc/webservice-demo
 
 kubectl apply -f kubernetes
