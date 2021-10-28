b8e01829047d8a05bc7b7993aaeca1e61f9cda7d
Gabriel <ggabmc@hotmail.com>
2021-10-27 18:34:43 -0700

    code change

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
-docker build --tag=ggabmc/webservice-demo -f ./Dockerfile .
+#docker build --tag=ggabmc/webservice-demo -f ./Dockerfile .
+docker build --tag=ggabmc/webservice-demo:latest --tag=ggabmc/webservice-demo:$SHA -f ./Dockerfile .
 
-docker push ggabmc/webservice-demo
+#docker push ggabmc/webservice-demo
+docker push ggabmc/webservice-demo:latest
+docker push ggabmc/webservice-demo:$SHA
 
 kubectl apply -f kubernetes
+
+kubectl set image deployments/ws-deployment ws=ggabmc/webservice-demo:$SHA
