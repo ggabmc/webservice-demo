20dbad62571879d3fcd38873d515bc777bcb9eb0
Gabriel <ggabmc@hotmail.com>
2021-11-02 22:23:40 -0700

    integrating cloud code plugin along with intellij

--- src/main/java/com/webservice/ControllerService.java
+++ src/main/java/com/webservice/ControllerService.java
@@ -1,6 +1,8 @@
 package com.webservice;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,8 @@ import java.util.List;
 
 @RestController
 public class ControllerService {
+
+    Logger logger = LoggerFactory.getLogger(ControllerService.class);
 
     String[] brands = {"Rolex","Omega","Tag Heuer","Hublot","Gucci","Cartier"};
 
@@ -20,20 +24,26 @@ public class ControllerService {
 
     @GetMapping("/healthcheck")
     public String healthCheck(){
-        return "webservice-demo is up and running!";
+        String message = "webservice-demo is up and running!";
+        logger.info(message);
+        return message;
     }
 
     @GetMapping("/watch/brands")
     public List getBrands(){
+        logger.info(brands.toString());
         return Arrays.asList(brands);
     }
 
     @GetMapping("/watch/rolex")
     public List getRolexModels(){
+        logger.info(rolex.toString());
         return Arrays.asList(rolex);
     }
 
     @GetMapping("/watch/omega")
-    public List getOmegaModels(){ return Arrays.asList(omega);}
+    public List getOmegaModels(){
+        logger.info(omega.toString());
+        return Arrays.asList(omega);}
 
 }
