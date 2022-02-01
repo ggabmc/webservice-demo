23f6d203bfa650e3233b6bd4ab82bbef4bb0862b
Gabriel <ggabmc@hotmail.com>
2022-01-31 20:21:05 -0800

    kubernetes documented

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-docker build --tag=ggabmc/webservice-demo:latest --tag=ggabmc/webservice-demo:$SHA -f ./Dockerfile .
-
-docker push ggabmc/webservice-demo:latest
-docker push ggabmc/webservice-demo:$SHA
-
-kubectl apply -f kubernetes
-
-kubectl set image deployments/ws-deployment webservice=ggabmc/webservice-demo:$SHA
+#docker build --tag=ggabmc/webservice-demo:latest --tag=ggabmc/webservice-demo:$SHA -f ./Dockerfile .
+#
+#docker push ggabmc/webservice-demo:latest
+#docker push ggabmc/webservice-demo:$SHA
+#
+#kubectl apply -f kubernetes
+#
+#kubectl set image deployments/ws-deployment webservice=ggabmc/webservice-demo:$SHA
 
 
 
